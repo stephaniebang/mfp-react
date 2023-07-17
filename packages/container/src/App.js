8d2e60e796164e2805d63045f3bfd66cce344979
stephaniebang <ej.bang27@gmail.com>
2023-07-17 15:58:50 -0300

    fix material ui styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import MarketingApp from '../components/MarketingApp';
+import Header from '../components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Container</h1>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
 
-      <hr />
-
-      <MarketingApp />
-    </div>
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
