dda517f4aca8d292f180a6554498c218aa7bda8d
stephaniebang <ej.bang27@gmail.com>
2023-07-26 09:04:24 -0300

    fix app rendering

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
 
   if (devRoot) {
-    mount(devRoot);
+    mountApp(devRoot);
   }
 }
 
